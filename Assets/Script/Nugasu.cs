e420c9cb49540ede4394a281abbdcad677d8f1c2
TakayaOga <akitsubame010@gmail.com>
2016-08-27 22:39:31 +0900

    仮スクリプトの追加

---
+++ Assets/Script/Nugasu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nugasu : MonoBehaviour
+{
+
+    //true 服がある
+    //false 服がない
+    static public bool tsirt;
+
+    // Use this for initialization
+    void Start()
+    {
+        tsirt = true;
+    }
+
+    
+    void Update()
+    {
+        if (tsirt == false)
+        {
+            transform.Translate(10, 0, 0);
+            tsirt = true;
+        }
+
+    }
+
+}
---
+++ Assets/Script/NugasuButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NugasuButton : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick()
+    { // MUST public
+        Nugasu.tsirt = false;
+        Debug.Log("clicked");
+    }
+}
