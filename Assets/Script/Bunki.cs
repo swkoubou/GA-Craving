ba81a047c02f92fc012992c41c5d3d97c2fe59d1
ookumaneko <asraimu9024@gmail.com>
2016-08-28 10:11:09 +0900

    タイトルからの分岐を追加

---
+++ Assets/Script/Bunki.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Bunki : MonoBehaviour {
+    public void Girl1()
+    {
+        SceneManager.LoadScene("yakyuken");
+    }
+    public void Girl2()
+    {
+        SceneManager.LoadScene("yakyuken2");
+    }
+}
